2d9a3234a206955caa9be5086823a976b70ef9d8
supraja_N <supraja_N@ganne-supraja>
2019-06-24 21:59:09 +0530

    commit done

---
+++ Fundamentals/src/DemoPackage/Demo3.java
@@ -0,0 +1,11 @@
+package DemoPackage;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+(a+b));
+		
+	}
+
+}
