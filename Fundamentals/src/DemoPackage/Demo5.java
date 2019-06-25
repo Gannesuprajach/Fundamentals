cba1995070fb7e94b76806a64a646e8cafc0507c
supraja_N <supraja_N@ganne-supraja>
2019-06-25 10:19:34 +0530

    commit done

---
+++ Fundamentals/src/DemoPackage/Demo5.java
@@ -0,0 +1,17 @@
+package DemoPackage;
+import java.util.*;
+
+public class Demo5 {
+	public static void main(String[] args) {
+		int i;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter a value:");
+		i=sc.nextInt();
+		if(i%2==0)
+			System.out.println("even number");
+		else
+			System.out.println("odd number");
+		
+	}
+
+}
