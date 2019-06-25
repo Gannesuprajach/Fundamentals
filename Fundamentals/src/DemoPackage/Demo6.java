e0e36fd16fd6e2893027010127712c851128f54b
supraja_N <supraja_N@ganne-supraja>
2019-06-25 10:40:21 +0530

    commit done

---
+++ Fundamentals/src/DemoPackage/Demo6.java
@@ -0,0 +1,11 @@
+package DemoPackage;
+
+public class Demo6 {
+	public static void main(String[] args) {
+		if(args.length>0)
+		System.out.println(args[0]+" , "+args[1]);
+		else
+		System.out.println("no values");
+	}
+
+}
